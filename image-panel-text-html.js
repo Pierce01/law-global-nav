5aeca8d76dd24cc26099c4b63ee5c08a4495df2d
vicchimenti <victor.chimenti@gmail.com>
2021-02-25 13:57:00 -0800

    card blocks

--- image-panel-text-html.js
+++ image-panel-text-html.js
@@ -45,6 +45,10 @@ try {
     var imageString = '<img class="card-image-top" src="' + backgroundImage +'">';
     var openOverlay = '<div class="card-img-overlay card-inverse">';
     var closeOverlay = '</div>';
+    var openBlock = '<div class="card-block">';
+    var closeBlock = '</div>';
+
+
     var beginningHTML = '<div class="panelWrapper contentItem noGap card shadow col-12" title="' + itemTitle + '" id="id<t4 type=\'meta\' meta=\'content_id\' data-position-default="Main" data-position-selected="Main" role="presentation" aria-labelledby="label<t4 type=\'meta\' meta=\'content_id\' />"/>">';
     var endingHTML = '</div>';
 
