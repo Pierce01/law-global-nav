0079c7ba5d53acb1ad1873c9c2ac48b1fead3fd8
vicchimenti <victor.chimenti@gmail.com>
2021-02-25 14:08:34 -0800

    description string

--- image-panel-text-html.js
+++ image-panel-text-html.js
@@ -47,8 +47,11 @@ try {
     var closeOverlay = '</div>';
     var openBlock = '<div class="card-block">';
     var closeBlock = '</div>';
-
-
+    var btnLinkOne = '<a href="' + btnOneStdLink + '" class="card-link">' + btnOneText + '</a>';
+    var btnLinkTwo = '<a href="' + btnTwoStdLink + '" class="card-link">' + btnTwoText + '</a>';
+    var openLinksList = '<ul class="panelLinks">';
+    var closeLinksList = '</ul>';
+    var descriptionString = '<p class="card-text">' + description + '</p>';
     var beginningHTML = '<div class="panelWrapper contentItem noGap card shadow col-12" title="' + itemTitle + '" id="id<t4 type=\'meta\' meta=\'content_id\' data-position-default="Main" data-position-selected="Main" role="presentation" aria-labelledby="label<t4 type=\'meta\' meta=\'content_id\' />"/>">';
     var endingHTML = '</div>';
 
