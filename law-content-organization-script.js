9455cf8845cedaff1a875d47c42831841e4106d6
vicchimenti <victor.chimenti@gmail.com>
2021-08-09 09:16:53 -0700

    rm cm cd

--- law-content-organization-script.js
+++ law-content-organization-script.js
@@ -26,11 +26,7 @@ var currentPageWidth = "xs";  //default since all content loads in ZoneA until m
 var listOfAllContentItems = $(".contentItem");
 var MainZoneContentItems = $.fn.add.call($(".contentItem[data-position-selected='Main']"),$(".contentItem[data-position-default='Main'][data-position-selected='']"));      
 var SidebarZoneContentItems = $.fn.add.call($(".contentItem[data-position-selected='Sidebar']"),$(".contentItem[data-position-default='Sidebar'][data-position-selected='']"));      
-	//var zoneCContentItems = $.fn.add.call($(".contentItem[data-position-selected='ZoneC']"),$(".contentItem[data-position-default='ZoneC'][data-position-selected='']"));      
-	//var zoneBandCContentItems = $.fn.add.call(zoneBContentItems,zoneCContentItems);      
-  //console.log("Number of Total Content Items: " + listOfAllContentItems.length);
-  //console.log("Number of Main Zone Content Items: " + MainZoneContentItems.length);
-  //console.log("Number of Sidebar Zone Content Items: " + SidebarZoneContentItems.length);
+
 /* function to determine the width of the page. Will run on page load 
    (line 45 below) and whenever the page is resized (reference in page layout) 
    result stored as bootsrap class (string) because it's easier to follow the 
@@ -70,14 +66,7 @@ MainZoneContentItems.sort(function(a, b){
 SidebarZoneContentItems.sort(function(a, b){
 	return $(a).data("order")-$(b).data("order")
 });      
-/*
-zoneCContentItems.sort(function(a, b){
-	return $(a).data("order")-$(b).data("order")
-});        
-zoneBandCContentItems.sort(function(a, b){
-	return $(a).data("order")-$(b).data("order")
-});
-*/
+
 
 // sitenav location is determined upon page load by nav objects
 if ($("#SidebarZone #siteNavigationWrapper").length){
@@ -99,7 +88,6 @@ if(currentPageWidth == "xs"){ /* xs */
 		moveNavToTopZone();
 	}
     $("#SidebarZone").hide();
-    //$("#zoneC").hide()
 }
 else if( currentPageWidth == "sm" ){/* if sm: put SidebarZone content items in SidebarZone. */
 	$("#MainZone").addClass("col-sm-12");
@@ -117,70 +105,32 @@ else if( currentPageWidth == "sm" ){/* if sm: put SidebarZone content items in S
 	}  
 }
 else if( currentPageWidth == "md-lg") { /* if md and lg: put content items in SidebarZone */
-	//console.log("md-lg else if");
     if (siteNavCurrentlyInSidebarZone){ 
-      //console.log("siteNavCurrentlyInSidebarZone" + siteNavCurrentlyInSidebarZone);
 		$("#SidebarZone").addClass("col-md-4"); /* do no matter what */
         	//if ((SidebarZoneContentItems.length == 0)){ /* no SidebarZone content */
 				$("#MainZone").addClass("col-md-8");
         $("#SidebarZone").show().append(SidebarZoneContentItems).addClass("col-md-4");
-				//$("#zoneB").hide();
-			//}
-            //else if ((zoneBContentItems.length > 0) && (zoneCContentItems.length > 0)){ /* and if something in zone B AND C */
-			//	$("#zoneA").addClass("col-md-6");
-            //  	$("#zoneB").show().addClass("col-md-3").append(zoneBContentItems);
-            //  	$("#zoneC").append(zoneCContentItems);	
-            //}
-			//else if (zoneBContentItems.length > 0) { /* if something in zone B Only*/
-			//	$("#zoneB").show().append(zoneBContentItems).addClass("col-md-3");
-			//	$("#zoneA").addClass("col-md-6"); /* since the menu is in Zone C */
-			//}
-          	//else if (zoneCContentItems.length > 0){ /* if something in zone C Only*/
-			//	$("#zoneC").append(zoneCContentItems);
-			//	$("#zoneA").addClass("col-md-9");
-            //}
+
 	}
 	else{ /* menu is in the topzone or nonexistent */
 
       if (SidebarZoneContentItems.length == 0){ /* no SidebarZone content */
 			$("#MainZone").addClass("col-md-12");
 			$("#SidebarZone").hide();
-           	//$("#zoneC").hide(); 
 		}
-       /*	else if ((zoneBContentItems.length > 0) && (zoneCContentItems.length > 0)){ */ /* and if something in zone B AND C */
-       /*    	$("#zoneB").show().addClass("col-md-3").append(zoneBContentItems);
-          	if ($("#zoneC").length){
-           		$("#zoneA").addClass("col-md-6");
-              	$("#zoneC").show().addClass("col-md-3").append(zoneCContentItems);	
-            }
-          	else{
-            	$("#zoneA").addClass("col-md-9");
-              	$("#zoneB").append(zoneCContentItems);	
-            }    
-        }*/
+
 		else if (SidebarZoneContentItems.length > 0) { /* if something in SidebarZone*/
 			$("#MainZone").addClass("col-md-8");
            	$("#SidebarZone").show().append(SidebarZoneContentItems).addClass("col-md-4");
-           	//$("#zoneC").hide(); 
 		}
-        //else if (zoneCContentItems.length > 0){ /* if something in zone C Only*/
-        //   	$("#zoneA").addClass("col-md-9");
-        //    if ($("#zoneC").length){ /* if zone C exists */
-        //      	$("#zoneC").show().addClass("col-md-3").append(zoneCContentItems);	/* put C things in zone C */
-        //    }
-        //  	else{ /* if it doesn't exist */
-        //      	$("#zoneB").append(zoneCContentItems);	/* put C things in zone B */
-        //    }
-        //}	
+
     }
 }
 
 /* function that reorgaizes content on the page based on screen width using 
    arrays of content items from above into the correct zones */
 function reorganizeContentItems(){
-	//console.log("currentPageWidth");
     if(currentPageWidth == "xs") {
-      //console.log("currentPageWidth");
       if (siteNavCurrentlyInSidebarZone){
 			moveNavToTopZone();
 		} 
@@ -215,51 +165,23 @@ function reorganizeContentItems(){
             	//$("#SidebarZone").show().addClass("col-md-3"); 
           	}
             $("#MainZone").addClass("col-md-8");
-        	//if ((SidebarZoneContentItems.length == 0)){ /* no zone C content, no zone B content */
-			//	$("#MainZone").addClass("col-md-9");
-				//$("#zoneB").hide();
-			//}
-          	//else if ((zoneBContentItems.length > 0) && (zoneCContentItems.length > 0)){ /* and if something in zone B AND C */
-			//	$("#zoneA").addClass("col-md-6");
-            //  	$("#zoneB").show().addClass("col-md-3").append(zoneBContentItems);
-            //  	$("#zoneC").append(zoneCContentItems);	
-            //}
-			//else if (zoneBContentItems.length > 0) { /* if something in zone B Only*/
-			//	$("#zoneA").addClass("col-md-6"); /* since the menu is in Zone C */
+        	
               	$("#SidebarZone").show().append(SidebarZoneContentItems).addClass("col-md-4");
-			//}
-          	//else if (zoneCContentItems.length > 0){ /* if something in zone C Only*/
-			//	$("#zoneA").addClass("col-md-9");
-            //  	$("#zoneC").append(zoneCContentItems);
-            //}
+
 		}
 		else{ /* menu is in the topzone or otherwise nonexistent */
 			if ((SidebarZoneContentItems.length == 0)){ /* no zone C content, no zone B content */
 				$("#MainZone").append(MainZoneContentItems).addClass("col-md-12");
 				$("#SidebarZone").hide();
-            	//$("#zoneC").hide(); 
 			}
           	else if ((SidebarZoneContentItems.length > 0)){ /* if something in SidebarZone */
               $("#SidebarZone").show().addClass("col-md-4").append(SidebarZoneContentItems);
-             // if ($("#zoneC").length){
-             //     $("#zoneA").addClass("col-md-6");
-              //    $("#zoneC").show().addClass("col-md-3").append(zoneCContentItems);	
-              //}
-              //else{
+
                   $("#MainZone").addClass("col-md-8");
               //    $("#SidebarZone").append(SidebarZoneContentItems);	
-              //} 	
+             	
             }
-			//else if (zoneBContentItems.length > 0) { /* if something in zone B Only*/
-			//	$("#zoneA").addClass("col-md-9");
-            //  	$("#zoneB").show().append(zoneBContentItems).addClass("col-md-3");
-            //	$("#zoneC").hide(); 
-            //}
-          	//else if (zoneCContentItems.length > 0){ /* if something in zone C Only*/
-			//	$("#zoneA").addClass("col-md-9");
-            //  	$("#zoneB").hide();
-            //  	$("#zoneC").show().addClass("col-md-3").append(zoneCContentItems);	
-            //}
+
         }
 	}
 	else{
