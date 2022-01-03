9b8b0e1703255526a2481a569db784c1d95180aa
Vic Chimenti <victor.chimenti@gmail.com>
2022-01-03 10:41:04 -0800

    clean

--- law-breadcrumbs.js
+++ law-breadcrumbs.js
@@ -1,4 +1,5 @@
-try{
+try {
+
     var channel = publishCache.getChannel(); //Channel information
     var sectionNames=[]; // array of names of parent sections
     var sectionIDs=[]; // array of IDs of parent sections
@@ -8,41 +9,57 @@ try{
     var mySection;
     var myLinkObject;
     var mySectionPath;
-    
-    if (sectionDepth > 2){ //do not output breadcrumbs on top-level Law pages
-   
+
+
+    //do not output breadcrumbs on top-level Law pages
+    if (sectionDepth > 2) {
+        
         //write the surrounding HTML for the breadcrumbs
         document.write('<div class="imageBannerBreadcrumbs">');
     
         //loop though section parents and append section names seperated by slashes
-      for (i = 0; i < sectionDepth; i++) { //loop through parent sections until rwaches current section level
+        for (i = 0; i < sectionDepth; i++) {
 
-        sectionNames[i] = (section.getName('en')); //name of the section in English ('en')
-        sectionIDs[i] = section.getID(); //id of the section (integer)
-        section = section.getParent(); //make the section the parent section. this makes T4 ascend the site structure every loop
+            sectionNames[i] = (section.getName('en')); //name of the section in English ('en')
+            sectionIDs[i] = section.getID(); //id of the section (integer)
+            section = section.getParent(); //make the section the parent section. this makes T4 ascend the site structure every loop
 
-        currentLoopSection  = com.terminalfour.publish.utils.TreeTraversalUtils.findSection(channel, section, sectionIDs[i], language);
-        currentLoopLinkObject = com.terminalfour.publish.PathBuilder.getLink(dbStatement, section, currentLoopSection, publishCache,language,true);
-        currentLoopSectionPath = currentLoopLinkObject.getLink();
-        sectionLinks[i] = currentLoopSectionPath; //link of the section
+            currentLoopSection  = com.terminalfour.publish.utils.TreeTraversalUtils.findSection(channel, section, sectionIDs[i], language);
+            currentLoopLinkObject = com.terminalfour.publish.PathBuilder.getLink(dbStatement, section, currentLoopSection, publishCache, language, true);
+            currentLoopSectionPath = currentLoopLinkObject.getLink();
+            
+            //link of the section
+            sectionLinks[i] = currentLoopSectionPath;
 
-      } //end the array builiding loop
-    
-      //reverse the names and links arrays to make the breadcrumbs start with ancestors first, then descend
-      sectionNames.reverse();
-      sectionLinks.reverse();
-      sectionNames[0] = 'Home'; //override name of the homepage section with Home
-      for (j = 0; j < sectionNames.length-1; j++) { // omit the current name and link section with -1
-        document.write('<a href="' + sectionLinks[j] + '">' + sectionNames[j] + '</a>'); 
-        if (j < sectionNames.length-2){ //omit the last slash in the output
-            document.write(' / ');
         }
-      } //end the output loop
-      document.write("</div>"); //close the .imageBannerBreadcrumbs div
-    } // end if statement 
-  } //try ends
-  
-  catch (err) {
+    
+        //reverse the names and links arrays to make the breadcrumbs start with ancestors first, then descend
+        sectionNames.reverse();
+        sectionLinks.reverse();
+
+        //override name of the homepage section with Home
+        sectionNames[0] = 'Home';
+
+
+
+        // omit the current name and link section with -1
+        for (j = 0; j < sectionNames.length-1; j++) {
+
+            document.write('<a href="' + sectionLinks[j] + '">' + sectionNames[j] + '</a>');
+
+            //omit the last slash in the output
+            if (j < sectionNames.length-2){
+                document.write(' / ');
+            }
+        }
+
+      document.write("</div>");
+
+
+
+
+    }
+  } catch (err) {
     document.write(err.message);
   }
   
