185e98d5bfd74cbcb51fc70b8728d84e28068e10
Vic Chimenti <victor.chimenti@gmail.com>
2022-01-08 15:32:34 -0800

    draw

--- homepage-map.js
+++ homepage-map.js
@@ -144,6 +144,30 @@ function initialize() {
 
 
 
+    SULawOverlay.prototype.draw = function() {
+        // We use the south-west and north-east
+        // coordinates of the overlay to peg it to the correct position and size.
+        // To do this, we need to retrieve the projection from the overlay.
+        var overlayProjection = this.getProjection(); // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+    
+        var sw = overlayProjection.fromLatLngToDivPixel(
+          this.bounds_.getSouthWest()
+        );
+        var ne = overlayProjection.fromLatLngToDivPixel(
+          this.bounds_.getNorthEast()
+        ); // Resize the image's div to fit the indicated dimensions.
+    
+        var div = this.div_;
+        div.style.left = sw.x + "px";
+        div.style.top = ne.y + "px";
+        div.style.width = ne.x - sw.x + "px";
+        div.style.height = sw.y - ne.y + "px";
+    };
+
+
+
 
     //Map style end
     //Map markers start
